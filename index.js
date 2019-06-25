ff830480063b61b7739c95b6b8ccc9b952d598a0
Vlad1slav <svlad1999@ukr.net>
2019-06-25 16:29:38 +0300

    HTTP SERVER

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const http = require('http');
+
+const routes = require('./src/router.js')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const router = {
+    '/': 'Hello World\n',
+    '/create': routes.create(),
+    '/read': routes.read(),
+    '/update': routes.update(),
+    '/delete': routes.myDelete()
+}
+
+const server = http.createServer((req, res) => {
+  const data = router[req.url];
+  res.end(data);    
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+server.on('error', err => {
+  if (err.code === 'EACCES') {
+    console.log(`No access to port: ${port}`);
+  }
+});
---
+++ src/router.js
@@ -0,0 +1,11 @@
+const create = () => 'I was created';
+const read = () => 'I was read';
+const update = () => 'I was updated';
+const myDelete = () => 'I was delete';
+
+module.exports = {
+    create,
+    read,
+    update,
+    myDelete
+}
